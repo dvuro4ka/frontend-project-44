458080b8050fd9f575effa9dd6b7e1493e9f0f82
Алексей Соломатин <nextroit@gmail.com>
2022-11-29 15:44:00 +0500

    create new game Arithmetic Progresion

---
+++ src/games/progression.js
@@ -0,0 +1,27 @@
+import game from '../index.js';
+
+const gameDesc = 'What number is missing in the progression?';
+
+const ArithProgression = (step, length, RandomNumber) => {
+  const result = [];
+
+  for (let i = 0, count = step; i <= length; i += 1, count += step) {
+    (i === RandomNumber) ? result.push('...') : result.push(`${count}`);
+  }
+  const stringResult = result.join(' ');
+
+  return stringResult;
+};
+
+export const progression = () => {
+  const step = Math.floor(Math.random() * 20 + 1);
+  const length = Math.floor(Math.random() * (20 - 5) + 5);
+  const RandomNumber = Math.floor(Math.random() * (10 - 1) + 1);
+  const question = ArithProgression(step, length, RandomNumber);
+  const rightAnswer = String(RandomNumber * step + step);
+  return [question, rightAnswer];
+};
+
+game(gameDesc, progression);
+
+export default progression;
