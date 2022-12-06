0d22fadefda4d40250f364f431c118f6cfa87c62
Алексей Соломатин <nextroit@gmail.com>
2022-12-06 10:20:51 +0500

    fixed bug when random number > length in progression

--- src/games/progression.js
+++ src/games/progression.js
@@ -4,7 +4,7 @@ const gameDescription = 'What number is missing in the progression?';
 
 const ArithProgression = (step, length, RandomNumber) => {
   const result = [];
-   
+
   for (let i = 0, count = step; i < length; i += 1, count += step) {
     if (i === RandomNumber) { result.push('..'); } else { result.push(`${count}`); }
   }
@@ -15,8 +15,9 @@ const ArithProgression = (step, length, RandomNumber) => {
 
 export const progression = () => {
   const step = Math.floor(Math.random() * 20 + 1);
-  const length = Math.floor(Math.random() * (20 - 5) + 5);
-  const RandomNumber = Math.floor(Math.random() * (10 - 1) + 1);
+  let length = Math.floor(Math.random() * (20 - 5) + 5);
+  let RandomNumber = Math.floor(Math.random() * (10 - 1) + 1);
+  if (RandomNumber > length) [length, RandomNumber] = [RandomNumber, length];
   const question = ArithProgression(step, length, RandomNumber);
   const rightAnswer = String(RandomNumber * step + step);
   return [question, rightAnswer];
